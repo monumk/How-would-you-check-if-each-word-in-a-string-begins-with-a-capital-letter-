214f44a8c18aae338dbdde17491895489bd0bd62
monumk <73218094+monumk@users.noreply.github.com>
2021-06-26 23:04:28 +0530

    Add files via upload

---
+++ title.py
@@ -0,0 +1,9 @@
+'''How would you check if each word in a string begins with a capital letter?'''
+ 
+ 
+s1='Govt Sr Sec School Faridabad'
+print(s1.istitle())
+
+
+s2='Govt Sr sec School faridabad'
+print(s2.istitle())
